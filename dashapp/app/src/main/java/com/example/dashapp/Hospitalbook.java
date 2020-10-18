5325368c023a50449592f276d223364259bdcc62
Ashutosh Gautam <ashutoshgautam9628@outlook.com>
2020-10-18 22:15:28 +0530

    Add files via upload

---
+++ dashapp/app/src/main/java/com/example/dashapp/Hospitalbook.java
@@ -0,0 +1,54 @@
+package com.example.dashapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class Hospitalbook extends AppCompatActivity {
+    TextView tvdate;
+    DatePickerDialog.OnDateSetListener setListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospitalbook);
+
+        tvdate=findViewById(R.id.textViewselctdate);
+
+
+        Calendar calendar=Calendar.getInstance();
+        final int year=calendar.get(Calendar.YEAR);
+        final int month=calendar.get(Calendar.MONTH);
+        final int day=calendar.get(Calendar.DAY_OF_MONTH);
+
+        tvdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                DatePickerDialog datePickerDialog=new DatePickerDialog(
+                        Hospitalbook.this, android.R.style.Theme_Holo_Dialog_MinWidth
+                        ,setListener,year,month,day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+        setListener=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month=month+1;
+                String date=day+"/"+month+"/"+year;
+                tvdate.setText(date);
+
+            }
+        };
+    }
+}
+
