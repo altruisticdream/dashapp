08b93d532095e75f8173c8c682db0a595f4e71df
Ashutosh Gautam <ashutoshgautam9628@outlook.com>
2020-10-18 03:13:01 -0700

    Update appointment.java

--- dashapp/app/src/main/java/com/example/dashapp/appointment.java
+++ dashapp/app/src/main/java/com/example/dashapp/appointment.java
@@ -2,13 +2,55 @@ package com.example.dashapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class appointment extends AppCompatActivity {
+    TextView tvdate;
+    EditText etdate;
+    DatePickerDialog.OnDateSetListener setListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment);
+        
+        
+         tvdate=findViewById(R.id.textView15);
+        etdate=findViewById(R.id.editTextTextPersonName7);
+
+        Calendar calendar=Calendar.getInstance();
+        final int year=calendar.get(Calendar.YEAR);
+        final int month=calendar.get(Calendar.MONTH);
+        final int day=calendar.get(Calendar.DAY_OF_MONTH);
+
+        tvdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog=new DatePickerDialog(
+                        appointment.this, android.R.style.Theme_Holo_Dialog_MinWidth
+                        ,setListener,year,month,day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+        setListener=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month=month+1;
+                String date=day+"/"+month+"/"+year;
+                tvdate.setText(date);
+
+            }
+        };
     }
 }
